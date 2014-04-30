b6f4c33098b9594fadb03e63aa92d71b2de3642e
theoretick <me@lucascharles.me>
2014-04-30 08:48:16 -0700

    p.27

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -26,7 +26,7 @@
    ; here we create a form with text fields called "name" and "message"
    ; these will be sent when the form posts to the server as keywords of
    ; the same name
-   (:form-to [:post "/"]
+   (form-to [:post "/"]
     [:p "Name:"]
     (text-field "name" name)
     [:p "Message:"]
@@ -34,21 +34,23 @@
     [:br]
     (submit-button "comment"))))
 
-;(defn home [& [name message error]] (layout/common
-;[:h1 "Guestbook"]
-;[:p "Welcome to my guestbook"]
-;[:p error]
-;;here we call our show-guests function
-;;to generate the list of existing comments
-;(show-guests)
-;[:hr]
-;;here we create a form with text fields called "name" and "message" ;these will be sent when the form posts to the server as keywords of ;the same name
-;(form-to [:post "/"]
-;[:p "Name:"]
-;(text-field "name" name)
-;[:p "Message:"]
-;(text-area {:rows 10 :cols 40} "message" message) [:br]
-;(submit-button "comment"))))
+; define the save-message function, which takes 2 params
+; using a `cond` (like a case statement), checks if 1st empty or
+; 2nd empty. If either empty, add errors. Else, print both and call home
+(defn save-message [name message]
+  (cond
+   (empty? name)
+   (home name message "Some Dummy forgot their name")
+   (empty? message)
+   (home name message "Someone forgot their messsage")
+   :else
+   (do
+     (println name message)
+     (home))))
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (GET "/" [] (home))
+  ; save-message checks that both name and message params are set and call
+  ; home function
+  ; NEED TO RESTART FOR ROUTES TO TAKE EFFECT
+  (POST "/" [name message] (save-message name message)))
